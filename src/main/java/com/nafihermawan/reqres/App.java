b5e549cf4f82c56ed4db2ba58cae88bcd9ee6370
Nafi Hermawan <nafi.konsultan@work.bri.co.id>
2022-03-28 16:48:54 +0700

    first commit

---
+++ src/main/java/com/nafihermawan/reqres/App.java
@@ -0,0 +1,13 @@
+package com.nafihermawan.reqres;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
